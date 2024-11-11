cb08bb1ec717784aade6b1e9d8aa4f3f3a056650
raf <rraf@tuta.io>
2024-11-11 17:14:13 +0000

    update: improved import export

--- flake.nix
+++ flake.nix
@@ -16,17 +16,42 @@
       system:
       let
         pkgs = import nixpkgs { inherit system; };
-        backend = pkgs.writeShellScriptBin "backend" ''
-          #!${pkgs.bash}/bin/bash
-          cd $PROJECT_DIR/backend/
-          pnpm install
-          pnpm run develop
-        '';
+
         frontend = pkgs.writeShellScriptBin "frontend" ''
           #!${pkgs.bash}/bin/bash
           cd $PROJECT_DIR/frontend/
           pnpm install
           pnpm run develop
+        '';
+
+        backend = pkgs.writeShellScriptBin "backend" ''
+          #!${pkgs.bash}/bin/bash
+          cd $PROJECT_DIR/backend/
+          pnpm install
+          pnpm strapi develop
+        '';
+
+        export-database = pkgs.writeShellScriptBin "export-database" ''
+          #!${pkgs.bash}/bin/bash
+          cd $PROJECT_DIR/backend/
+          pnpm install
+          pnpm strapi export
+          cd $PROJECT_DIR
+          mkdir -p database_backups
+          mv backend/export_*.tar.gz.enc database_backups/
+        '';
+
+        import-database = pkgs.writeShellScriptBin "import-database" ''
+          #!${pkgs.bash}/bin/bash
+          cd $PROJECT_DIR/backend/
+          echo $PROJECT_DIR/database_backups/
+          ls $PROJECT_DIR/database_backups/
+          if [ -z "$1" ]; then
+          echo "No backup file provided."
+          exit 1
+          fi
+          pnpm install
+          pnpm strapi import --file "$1"
         '';
       in
       {
@@ -34,8 +59,11 @@
           buildInputs = [
             pkgs.nodejs
             pkgs.pnpm
-            backend
+            pkgs.dialog
             frontend
+            backend
+            export-database
+            import-database
           ];
           shellHook = ''
             export PROJECT_DIR="$(pwd)"
