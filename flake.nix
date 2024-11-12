ebf3392d1a128166ef54a0066e6ebb640e9cf1f0
raf <rraf@tuta.io>
2024-11-12 17:56:05 +0000

    update: added environment variables for dev environment

--- flake.nix
+++ flake.nix
@@ -73,6 +73,22 @@
           ];
           shellHook = ''
             export PROJECT_DIR="$(pwd)"
+            export HOST=0.0.0.0
+            export PORT=1337
+            export APP_KEYS=TRlbMBSMOZ1TQV5CevOjAA==,x3nHmUiDhib8NeTtDDanjg==,/HeZIEYgTN4XhClU7E7pgA==,3II6NqXxNq3Av0X2xkYH6Q==
+            export API_TOKEN_SALT=OIVz+gPSkAWc6aqBh+sW7A==
+            export ADMIN_JWT_SECRET=xVp1ubf+HHc+JZVQ+m9NEA==
+            export TRANSFER_TOKEN_SALT=OCkd7KJ6zU2K4RpEaCQA4A==
+            export DATABASE_CLIENT=sqlite
+            export DATABASE_HOST=
+            export DATABASE_PORT=
+            export DATABASE_NAME=
+            export DATABASE_USERNAME=
+            export DATABASE_PASSWORD=
+            export DATABASE_SSL=false
+            export DATABASE_FILENAME=.tmp/data.db
+            export JWT_SECRET=APHvSmKM5qD68FY8utznJQ==
+
             echo
             echo "2024, Sheffield Hallam University, Software Architecture & Design, Task 2: Proof of Concept. Development Shell. Configuration written by Raf Shahid: C0042249"
             echo
