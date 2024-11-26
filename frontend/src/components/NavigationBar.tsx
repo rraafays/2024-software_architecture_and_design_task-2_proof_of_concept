a70e2e27cd68f847d6352705db8aec9358267bcd
raf <rraf@tuta.io>
2024-11-26 09:04:02 +0000

    fix: made logo sit next to logo text

--- frontend/src/components/NavigationBar.tsx
+++ frontend/src/components/NavigationBar.tsx
@@ -4,48 +4,19 @@ import Image from "next/image";
 
 const NavigationBar = () => {
   return (
-    <div
-      className={`
-		  h-20
-		  px-4
-		  md:px-8
-		  lg:px-16
-		  xl:px-32
-		  2xl:px-64
-		  relative
-		`}
-    >
-      <div
-        className={`
-			md:hidden
-			h-full
-			flex
-			items-center
-			justify-between
-		  `}
-      >
+    <div className="h-20 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 relative">
+      <div className="md:hidden h-full flex items-center justify-between">
         <Link href="/">
           <div className={`text-2xl tracking-wide`}>Advanced Media Library</div>
         </Link>
         <Menu />
       </div>
 
-      <div
-        className={`
-			hidden
-			md:flex
-			items-center
-			h-full
-			justify-center
-			gap-8
-		  `}
-      >
-        <div className="w-1/3">
-          <Link href="/">
+      <div className="md:flex hidden items-center justify-between gap-8 h-full">
+        <div className="w-1/3 xl:w-1/2 flex items-center gap-12">
+          <Link href="/" className="flex items-center gap-3">
             <Image src="/logo.png" alt="logo" width={24} height={24} />
-            <div className={`text-2xl tracking-wide`}>
-              Advanced Media Library
-            </div>
+            <div className="text-2xl tracking-wide">Advanced Media Library</div>
           </Link>
         </div>
         <div className="w-2/3"></div>
