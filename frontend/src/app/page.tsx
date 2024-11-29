a2e24dcb0ce238d22d380aa3e04a74cf1de58ef2
raf <rraf@tuta.io>
2024-11-29 14:33:31 +0000

    update: wrote slider

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -7,7 +7,9 @@ const HomePage = () => {
     <div className="">
       <Slider />
       <div className="mt-24">
-        <h1 className="text-2xl">Categories</h1>
+        <h1 className="text-2xl px-4 md:px-8 lg:px-16 xl:32 2xl:px-64 mb-12">
+          Categories
+        </h1>
         <CategoryList />
       </div>
       <div className="mt-24 px-4 md:px-8 lg:px-16 xl:32 2xl:px-64">
