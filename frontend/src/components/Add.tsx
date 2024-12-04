b9c7305536a8130333aaa678d94e0a48166a707c
raf <rraf@tuta.io>
2024-12-04 13:11:27 +0000

    update: wrote increment decrement logic

--- frontend/src/components/Add.tsx
+++ frontend/src/components/Add.tsx
@@ -5,21 +5,40 @@ import { useState } from "react";
 const Add = () => {
   const [quantity, setQuantity] = useState(1);
 
+  const incrementQuantity = (increment: false | true) => {
+    if (increment === false && quantity > 1) {
+      setQuantity((prev) => prev - 1);
+    }
+    if (increment === true && quantity < 99) {
+      setQuantity((prev) => prev + 1);
+    }
+  };
+
   return (
     <div className="flex flex-col gap-4">
       <h4 className="font-medium">Quantity</h4>
       <div className="flex justify-between">
         <div className="flex items-center gap-4">
           <div className="bg-gray-100 py-2 px-4 rounded-3xl flex items-center justify-between w-32">
-            <button className="cursor-pointer text-xl">-</button>
+            <button
+              className="cursor-pointer text-xl"
+              onClick={() => incrementQuantity(false)}
+            >
+              -
+            </button>
             {quantity}
-            <button className="cursor-pointer text-xl">+</button>
+            <button
+              className="cursor-pointer text-xl"
+              onClick={() => incrementQuantity(true)}
+            >
+              +
+            </button>
           </div>
         </div>
+        <button className="w-36 rounded-3xl ring-1 ring-pink text-pink py-2 px-4 hover:bg-pink hover:text-white disabled:cursor-not-allowed disabled:bg-gray-200 disabled:text-white disabled:ring-none">
+          Add to Basket
+        </button>
       </div>
-      <button className="w-36 rounded-3xl ring-1 ring-pink text-pink py-2 px-4 hover:bg-pink hover:text-white disabled:cursor-not-allowed disabled:bg-gray-200 disabled:text-white disabled:ring-none">
-        Add to Basket
-      </button>
     </div>
   );
 };
